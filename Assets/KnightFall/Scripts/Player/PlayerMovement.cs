bdbd4fd904766eddf2e3066ea2de8577ede14bd0
Meand05 <djay.verlaar@icloud.com>
2026-09-16 18:49:29 +0200

    S1-02 Player Movement Animations

--- Assets/KnightFall/Scripts/Player/PlayerMovement.cs
+++ Assets/KnightFall/Scripts/Player/PlayerMovement.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public int facingDirection = 1;
     public Rigidbody2D rb;
+    public Animator anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,7 +18,22 @@ public class PlayerMovement : MonoBehaviour
         float horizontalinput = Input.GetAxis("Horizontal");
         float verticalinput = Input.GetAxis("Vertical");
 
-        rb.linearVelocity = new Vector2(horizontalinput, verticalinput);
+        // Check if the player is moving left or right and flip the sprite accordingly
+        if (horizontalinput > 0 && transform.localScale.x <0 || horizontalinput < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        anim.SetFloat("horizontal", Mathf.Abs(horizontalinput));
+        anim.SetFloat("vertical", Mathf.Abs(verticalinput));
+
+        rb.linearVelocity = new Vector2(horizontalinput, verticalinput) * moveSpeed;
       
+    }
+    // Flips the player sprite horizontally
+    void Flip()
+    {
+       facingDirection *= -1;
+       transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
